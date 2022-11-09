9c2a3c6dbd32f8b88677623e0b7f86477d5308c9
ArmandoMartiny <71964707+ArmandoMartiny@users.noreply.github.com>
2022-11-08 21:05:59 -0600

    socket.io

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 import { PORT } from './config.js'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url';
-
+/* Realizado por armando Martinez */
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const server = http.createServer(app)
